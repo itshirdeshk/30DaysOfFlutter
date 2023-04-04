d6231b3ea5ce296bf28857eb61cd0ecefaaa15c8
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-04-04 12:49:55 +0530

    Day 27 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:practice_1/core/store.dart';
 import 'dart:convert';
 import 'package:practice_1/models/catalog.dart';
 import 'package:practice_1/utils/routes.dart';
@@ -8,6 +9,10 @@ import 'package:practice_1/widgets/home_widgets/catalog_header.dart';
 import 'package:practice_1/widgets/home_widgets/catalog_list.dart';
 import 'package:practice_1/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
+
+import '../models/cart.dart';
+
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -40,14 +45,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-        backgroundColor: MyThemes.brownishColor,
-        child: const Icon(
-          CupertinoIcons.cart,
-          color: Colors.white,
-        ),
+      floatingActionButton: VxBuilder(
+        mutations: {AddMutation, RemoveMutation},
+        builder: (context, _, __) => FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+          backgroundColor: MyThemes.brownishColor,
+          child: const Icon(
+            CupertinoIcons.cart,
+            color: Colors.white,
+          ),
+        ).badge(count: _cart.items.length),
       ),
       backgroundColor: context.canvasColor,
       body: SafeArea(
